61cb31ccde168bad34ef06c83fbb213f8a93e2a1
dottg0n20 <dev@dariocaruso.info>
2021-01-23 18:32:34 +0100

    ref

--- boot.ts
+++ boot.ts
@@ -28,19 +28,18 @@ const services = process.env.SERVICES.split(',').map(m => {
   }
 })
 for (const s of services) {
-  data += 'frontend ' + s.port + '_front\n\
-  bind *:' + s.port + '\n\
-  mode ' + s.mode + '\n' + (s.mode==='tcp' ? '  option tcp-check \n':'') +'\
-  default_backend ' + s.port + '_back\n\n'
+  data += 'frontend ' + s.port + '_front\n'
+  data += '  bind *:' + s.port + '\n'
+  data += '  mode ' + s.mode + '\n' + (s.mode === 'tcp' ? '  option tcp-check \n' : '')
+  data += '  default_backend ' + s.port + '_back\n\n'
 }
 for (const s of services) {
-  data += 'backend ' + s.port + '_back\n\
-  balance roundrobin\n'
+  data += 'backend ' + s.port + '_back\n'
+  data += '  balance roundrobin\n'
   for (const b of backends) {
-    data += '  server ' + b.replace(/\./g, '_') + ' ' + b + ':'+s.targetPort+' check\n'
+    data += '  server ' + b.replace(/\./g, '_') + ' ' + b + ':' + s.targetPort + ' check\n'
   }
-  data+='\n'
+  data += '\n'
 }
-
 
 fs.writeFileSync(process.env.HAPROXY_CONFIG_PATH || '/usr/local/etc/haproxy/haproxy.cfg', data)
